8ce5dc66b217fbbc49c32d525fb04fae242d2fa6
enping <58761173@qq.com>
2021-05-01 13:45:00 +0800

    commit

---
+++ website-demo/src/test/java/com/jep/github/websitedemo/WebsiteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jep.github.websitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
