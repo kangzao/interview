7877a0843014dc3f392d3b69f4297aca7a3c93c1
enping.jep <58761173@qq.com>
2023-09-25 14:02:55 +0800

    juc

--- concurrency/src/main/java/com/jep/github/interview/concurrency/future/MyCallable.java
+++ concurrency/src/main/java/com/jep/github/interview/concurrency/future/MyCallable.java
@@ -30,7 +30,7 @@ public class MyCallable implements Callable<String> {
             Future<String> future = executor.submit(callable);
             //add Future to the list, we can get return value using Future
 //            list.add(future);
-            System.out.println(new Date() + "::" + future.get());//阻塞
+            System.out.println(new Date() + "::" + future.get() + ",currentThread ==" + Thread.currentThread().getName());//阻塞
         }
         //shut down the executor service now
         executor.shutdown();
