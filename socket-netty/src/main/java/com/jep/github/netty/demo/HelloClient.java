bcfd6622c3bc897ba5402e8890c9f7845f6c4f8e
enping.jep <58761173@qq.com>
2023-11-14 14:08:34 +0800

    netty

---
+++ socket-netty/src/main/java/com/jep/github/netty/demo/HelloClient.java
@@ -0,0 +1,37 @@
+package com.jep.github.netty.demo;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author enping.jep
+ * @date 2023/11/10 10:39
+ **/
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 1. 启动类
+        new Bootstrap()
+                // 2. 添加 EventLoop  创建 NioEventLoopGroup，可以简单理解为 线程池 + Selector
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端 channel 实现 选择服务 Scoket 实现类，其中 NioServerSocketChannel 表示基于 NIO 的服务器端实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override // 在连接建立后被调用
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8181))
+                .sync()
+                .channel()
+                // 6. 向服务器发送数据
+                .writeAndFlush("hello, world");
+    }
+}
