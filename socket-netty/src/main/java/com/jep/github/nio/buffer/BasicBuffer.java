3eb9de9aaf371bcec6f30d6d9ac41e4e5d8facca
enping <58761173@qq.com>
2024-05-17 21:17:21 +0800

    nio

--- socket-netty/src/main/java/com/jep/github/nio/buffer/BasicBuffer.java
+++ socket-netty/src/main/java/com/jep/github/nio/buffer/BasicBuffer.java
@@ -11,19 +11,16 @@ public class BasicBuffer {
         IntBuffer intBuffer = IntBuffer.allocate(5);
 
         //向buffer存放数据
-        //intBuffer.put(10);
-        //intBuffer.put(11);
-        //intBuffer.put(12);
-        //intBuffer.put(13);
-        //intBuffer.put(14);
+        //初始化过程中mark== -1  position == 0 limit == 5  capacity == 5
         for (int i = 0; i < intBuffer.capacity(); i++) {
-            intBuffer.put(i * 2);
+            intBuffer.put(i * 2);//position会不断增大
         }
         //如何从 buffer 读取数据
-        //将 buffer 转换，读写切换(!!!)
+        //将 buffer 转换，读写切换(!!!)  此时position会增大到5
         intBuffer.flip();
+        //此时pos == 0
         while (intBuffer.hasRemaining()) {
-            System.out.println(intBuffer.get());
+            System.out.println(intBuffer.get()); //position会增大到5
         }
     }
 }
