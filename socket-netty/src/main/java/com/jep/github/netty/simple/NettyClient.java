837ecea076c5e976c6229786bf2b9f0c96608930
kangzao <58761173@qq.com>
2024-06-25 11:35:17 +0800

    netty

--- socket-netty/src/main/java/com/jep/github/netty/simple/NettyClient.java
+++ socket-netty/src/main/java/com/jep/github/netty/simple/NettyClient.java
@@ -9,13 +9,13 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class NettyClient {
-    
+
     public static void main(String[] args) throws Exception {
 
         //客户端需要一个事件循环组
         EventLoopGroup group = new NioEventLoopGroup();
         try {
-            
+
             //创建客户端启动对象
             //注意客户端使用的不是 ServerBootstrap 而是 Bootstrap
             Bootstrap bootstrap = new Bootstrap();
@@ -28,7 +28,7 @@ public class NettyClient {
                             ch.pipeline().addLast(new NettyClientHandler()); //加入自己的处理器
                         }
                     });
-            
+
             System.out.println("客户端 ok..");
             //启动客户端去连接服务器端
             //关于 ChannelFuture 要分析，涉及到netty的异步模型
