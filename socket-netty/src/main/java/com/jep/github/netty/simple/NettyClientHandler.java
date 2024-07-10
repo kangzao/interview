77f5615e096ee414862dc53854b430077a706ee8
enping.jep <58761173@qq.com>
2024-07-10 21:20:36 +0800

    nio

--- socket-netty/src/main/java/com/jep/github/netty/simple/NettyClientHandler.java
+++ socket-netty/src/main/java/com/jep/github/netty/simple/NettyClientHandler.java
@@ -7,7 +7,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
 public class NettyClientHandler extends ChannelInboundHandlerAdapter {
-    
+
     //当通道就绪就会触发该方法
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -18,6 +18,7 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter {
     //当通道有读取事件时，会触发
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        int i = 1 / 0;
         ByteBuf buf = (ByteBuf) msg;
         System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
         System.out.println("服务器的地址： " + ctx.channel().remoteAddress());
@@ -25,6 +26,7 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("exceptionCaught");
         cause.printStackTrace();
         ctx.close();
     }
