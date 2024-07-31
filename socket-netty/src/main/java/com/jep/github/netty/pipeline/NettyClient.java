016a40f723156a9de4c4b762969ed469ad81cacf
kangzao <58761173@qq.com>
2024-07-31 21:15:43 +0800

    netty

--- socket-netty/src/main/java/com/jep/github/netty/pipeline/NettyClient.java
+++ socket-netty/src/main/java/com/jep/github/netty/pipeline/NettyClient.java
@@ -1,5 +1,7 @@
 package com.jep.github.netty.pipeline;
 
+import java.nio.charset.StandardCharsets;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -7,8 +9,6 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-
-import java.nio.charset.StandardCharsets;
 
 public class NettyClient {
 
@@ -29,11 +29,12 @@ public class NettyClient {
                         ChannelPipeline pipeline = ch.pipeline();
                         // client发送消息的顺序为CustomOutboundHandler1 -> CustomOutboundHandler2  只会执行OutboundHandler
                         pipeline.addLast(new CustomOutboundHandler2());
-                        pipeline.addLast(new CustomOutboundHandler1());
+//                        pipeline.addLast(new CustomOutboundHandler1());
 
                         //client接收到server返回的消息，执行顺序为  CustomInboundHandler2 -> CustomInboundHandler1
                         pipeline.addLast(new CustomInboundHandler2());
                         pipeline.addLast(new CustomInboundHandler1());
+
                         pipeline.addLast(new SimpleChannelInboundHandler<ByteBuf>() {
                             @Override
                             public void channelActive(ChannelHandlerContext ctx) {
@@ -43,8 +44,11 @@ public class NettyClient {
                                 firstMessage.writeBytes(req);
                                 System.out.println(this.getClass() + "------------ 开始发送消息 ------------");
                                 //在handler中调用ctx.writeAndFlush()方法后，就会将数据交给ChannelOutboundHandler进行出站处理
+                                //当调用ChannelHandlerContext的writeAndFlush方法时，这个调用点被视为出站事件的起点。
+                                //writeAndFlush方法会从当前ChannelHandler开始在pipeline中向前传播write事件，直到遇到HeadContext
                                 ctx.writeAndFlush(firstMessage);//ctx.writeAndFlush只会从当前的handler位置开始，往前找outbound执行
                             }
+
 
                             @Override
                             protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
@@ -58,8 +62,11 @@ public class NettyClient {
                             }
 
                         });
+
+                        pipeline.addLast(new CustomOutboundHandler1());
                     }
                 });
+
 
         ChannelFuture future = clientBootstrap.connect("localhost", 8188).sync();
         future.channel().closeFuture().sync();
